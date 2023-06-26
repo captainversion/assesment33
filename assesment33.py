b83400029723b9adc9f346e62505dd260cefd0e5
captainversion <136670723+captainversion@users.noreply.github.com>
2023-06-26 18:50:30 +0530

    Add files via upload

---
+++ assesment33.py
@@ -0,0 +1,13 @@
+def string(s):
+    a={"uppercase":0, "lowercase":0}
+    for b in s:
+        if b.isupper():
+           a["uppercase"]+=1
+        elif b.islower():
+           a["lowercase"]+=1
+        else:
+           pass
+    print ("No. of Upper case characters : ", a["uppercase"])
+    print ("No. of Lower case Characters : ", a["lowercase"])
+
+string('Ganesh')
